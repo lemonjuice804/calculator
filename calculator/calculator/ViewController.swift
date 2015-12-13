fc345e2cfdb723f0517028145300d9d26c516067
Jiayin Cheng <jicheng@groupon.com>
2015-12-12 16:07:32 -0800

    First commit. Added buttons and display label.

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Jiayin Cheng on 12/12/15.
+//  Copyright © 2015 Jiayin Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // property: instance variable
+    // all objects live in the heap. the memory for them is managed for you.
+    // '!' exclamation:
+    @IBOutlet weak var display: UILabel!
+    
+    // Action: Button click action.
+    @IBAction func appendDigit(sender: UIButton) {
+    }
+
+}
+
